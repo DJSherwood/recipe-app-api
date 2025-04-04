f94fd5ed79f32111729a8d71904f0b0621074bba
DJSherwood <sherwood.daniel@gmail.com>
2025-04-04 10:19:10 -0500

    attempt to fix liniting

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,6 +4,7 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
@@ -11,7 +12,6 @@ class CalcTests(SimpleTestCase):
         "Test adding numbers together"
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
