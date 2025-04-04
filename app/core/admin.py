3d13b227a38e2dd9742627c8daa5d6a16fc591ad
DJSherwood <sherwood.daniel@gmail.com>
2025-04-04 13:08:29 -0500

    adding admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,11 +6,12 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from core import models
 from django.utils.translation import gettext_lazy as _
 
+
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
-    fieldSets = (
+    fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (
             _('Permissions'),
@@ -25,5 +26,19 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 admin.site.register(models.User, UserAdmin)
